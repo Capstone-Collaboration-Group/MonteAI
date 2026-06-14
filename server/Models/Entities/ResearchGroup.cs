36cb7a6d768b8618aa9fe6caf4409e4a848da94d
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-14 08:45:59 +0800

    feat: added Group for students

---
+++ server/Models/Entities/ResearchGroup.cs
@@ -0,0 +1,11 @@
+﻿namespace server.Models.Entities
+{
+    public class ResearchGroup
+    {
+        public int? Id { get; set; }
+
+        public string? CreatedAt { get; set; }
+
+        public string? UpdatedAt { get; set; }
+    }
+}
