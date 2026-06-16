5753a1a38df79eca33c60605f783c7615fad1ec5
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-16 15:49:27 +0800

    feat: added entity model for Submissions

---
+++ server/Models/Entities/Submission.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models.Entities
+{
+    public class Submission
+    {
+        [Key]
+        public int? Id { get; set; }
+
+        [Required]
+        public string? ThesisId { get; set; }
+        [Required]
+        public string? StudentNumber { get; set; }
+
+        [Required]
+        public string? SubmittedAt { get; set; }
+
+        [MaxLength(999)]
+        public string? Notes { get; set; }
+
+
+    }
+}
