dee48c39ee31b8094578740a5a0b1af5e87136ad
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-16 18:12:27 +0800

    refactor: fixed nullable properties

--- server/Models/Entities/Thesis.cs
+++ server/Models/Entities/Thesis.cs
@@ -5,32 +5,37 @@ namespace server.Models.Entities
     public class Thesis 
     {
         [Key]
-        public string? Id { get; set; }
+        public Guid Id { get; set; }
 
         [Required]
         [MaxLength(255)]
         public string? Title{ get; set; }
-        [Required]
-        public List<Student>? Authors { get; set; }
+
+        public ICollection<Submission> Submissions { get; set; } = [];
 
         [Required]
         public string? Abstract { get; set; }
 
         [Required]
-        public string? FilePath { get; set; }
+        public string FilePath { get; set; } = string.Empty;
 
         [Required]
-        public string? UploadedById { get; set; }
+        public string UploadedById { get; set; } = string.Empty;
 
         [Required]
-        [MaxLength(100)]
-        public string? Status { get; set; }
-        
-        public string? PineconeStatus { get; set; }
+        [MaxLength(15)]
+        public string? Status { get; set; } = "Pending";
+
+        public string? PineconeStatus { get; set; } = "None";
 
         public DateTime? SubmittedAt { get; set; }
+        public DateTime? ReviewedAt { get; set; }
+        public DateTime? ApprovedAt { get; set; }
+        public DateTime? RejectedAt { get; set; }
+        public DateTime? IndexedAt { get; set; }
 
-        public DateTime? UpdatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
 
 
     }
