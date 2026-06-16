acb47c25150cf1e85d5a7c1589fc873c2955479d
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-16 15:50:02 +0800

    feat: added entity model for PanelistSchedule

---
+++ server/Models/Entities/Review.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models.Entities
+{
+    public class Review
+    {
+        [Key]
+        public int? Id { get; set; }
+
+        [Required]
+        public int? ThesisId { get; set; }
+
+        [Required]
+        public string?  ReviewerId { get; set; }
+
+        [Required]
+        public string? Decision { get; set; }
+
+        [MaxLength(1000)]
+        public string?  Comments { get; set; }
+
+        public DateTime? ReviewedAt { get; set; }
+
+    }
+}
