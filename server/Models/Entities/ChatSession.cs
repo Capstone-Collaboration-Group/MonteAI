4b58cb522af399b8add865c1350043fb47df02e4
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-16 15:50:25 +0800

    feat: added entity model for ChatSession

---
+++ server/Models/Entities/ChatSession.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models.Entities
+{
+    public class ChatSession
+    {
+        [Key]
+        public Guid? Id { get; set; }
+
+        [Required]
+        public string? UserId { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public DateTime? LastChatDate { get; set; }
+    }
+}
