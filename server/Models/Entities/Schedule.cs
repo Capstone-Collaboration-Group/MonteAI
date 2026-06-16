6c9915e8a9db32b1920652a28076cf53cd1202d5
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-16 15:49:43 +0800

    feat: added entity model for ChatSessions

---
+++ server/Models/Entities/Schedule.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models.Entities
+{
+    public class Schedule
+    {
+        [Key]
+        public Guid ScheduleId { get; set; } 
+
+        public string? ScheduledBy { get; set; }
+
+        public string? GroupId { get; set; }
+
+        public DateOnly Date { get; set; }
+
+        public TimeOnly StartTime { get; set; }
+
+        public TimeOnly EndingTime { get; set; }
+
+        public string? RoomVenue { get; set; } 
+
+        public string? AdditionalInformation { get; set; }
+        
+        public ICollection<PanelistSchedule>? Panelists { get; set; } 
+
+
+   
+    }
+}
