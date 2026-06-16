3c7e97f570a16e5e77422f827b7a423d2863b1b7
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-16 15:49:06 +0800

    feat: added entity model for Thesis

---
+++ server/Models/Entities/Thesis.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models.Entities
+{
+    public class Thesis 
+    {
+        [Key]
+        public string? Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string? Title{ get; set; }
+        [Required]
+        public List<Student>? Authors { get; set; }
+
+        [Required]
+        public string? Abstract { get; set; }
+
+        [Required]
+        public string? FilePath { get; set; }
+
+        [Required]
+        public string? UploadedById { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string? Status { get; set; }
+        
+        public string? PineconeStatus { get; set; }
+
+        public DateTime? SubmittedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+
+    }
+}
