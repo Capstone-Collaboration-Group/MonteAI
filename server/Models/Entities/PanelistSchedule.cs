6ba5c3a8f451b53adad3108415a3d26bbad39356
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-16 18:02:29 +0800

    refactor: fix panelist ID type

--- server/Models/Entities/PanelistSchedule.cs
+++ server/Models/Entities/PanelistSchedule.cs
@@ -4,10 +4,11 @@
     {
         public Guid ScheduleId { get; set; }
 
-        public Schedule? Schedule { get; set; } 
+        public Schedule? Schedule { get; set; }
 
-        public Guid PanelistId { get; set; } 
+        public string PanelistId { get; set; } = string.Empty;
 
-        public string PanelistType { get; set; }
+        public string? PanelistType { get; set; }
+
     }
 }
