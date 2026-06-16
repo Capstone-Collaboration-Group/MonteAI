ead824c7400187f9884ba9803654fbb766815496
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-16 18:01:11 +0800

    refactor: fixed wrong ID type

--- server/Models/Entities/ChatMessage.cs
+++ server/Models/Entities/ChatMessage.cs
@@ -1,6 +1,21 @@
-﻿namespace server.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models.Entities
 {
+    // will be stored in Firestore 
     public class ChatMessage
     {
+        [Required]
+        public string Id { get; set; } =string.Empty;
+
+        public string SessionId { get; set; } = string.Empty;
+
+        [Required]
+        public string? Role { get; set; } = string.Empty;
+
+        [Required]
+        public string? Content { get; set; } = string.Empty;
+
+        public DateTime? Timestamp { get; set; }
     }
 }
