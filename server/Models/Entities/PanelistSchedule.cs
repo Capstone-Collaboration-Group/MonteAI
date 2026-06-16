835a3b55b9a3bae2190d7b3abc694ddc847f282b
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-16 15:50:45 +0800

    feat: added entity model for Panelist Schedule

---
+++ server/Models/Entities/PanelistSchedule.cs
@@ -0,0 +1,13 @@
+﻿namespace server.Models.Entities
+{
+    public class PanelistSchedule
+    {
+        public Guid ScheduleId { get; set; }
+
+        public Schedule? Schedule { get; set; } 
+
+        public Guid PanelistId { get; set; } 
+
+        public string PanelistType { get; set; }
+    }
+}
