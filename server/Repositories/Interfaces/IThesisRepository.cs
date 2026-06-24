c0208430746dac0606a979c9a524f0d819834ea5
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-25 01:30:05 +0800

    feat: add Interface for data accessing class thesis

---
+++ server/Repositories/Interfaces/IThesisRepository.cs
@@ -0,0 +1,19 @@
+﻿using server.Models.Entities;
+
+namespace server.Repositories.Interfaces
+{
+    public interface IThesisRepository
+    {
+        Task<IEnumerable<Thesis>> GetFirst20ThesisAsync();
+
+        Task<Thesis?> GetThesisByIdAsync(Guid id);
+
+        Task<bool> UpdateDetailsAsync(Thesis thesis);
+
+        Task<bool> UpdateStatusAsync(Guid id, string status);
+
+        Task<bool> DeleteThesisAsync(Guid id);
+
+
+    }
+}
