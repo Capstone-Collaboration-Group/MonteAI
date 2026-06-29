6c8276a3790619156470b11c0ce2b2e2b029b138
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-29 10:14:34 +0800

    feat: add api controller endpoint for Authentication

---
+++ server/Repositories/Interfaces/IStudentRepository.cs
@@ -0,0 +1,18 @@
+﻿using server.Models.Entities;
+
+namespace server.Repositories.Interfaces
+{
+    public interface IStudentRepository
+    {
+        Task CreateAsync(Student student);
+        Task<Student?> GetByIdAsync(string id);
+
+        Task<IEnumerable<Student>> GetAllAsync();
+
+        Task UpdateAsync(Student student);
+
+
+
+        //Task DeleteAsync(string id);
+    }
+}
