18e1bf7e5025c51c71740db3be58dfde013cdf04
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-29 10:17:30 +0800

    feat: add interface for student service

---
+++ server/Services/Interfaces/IStudentService.cs
@@ -0,0 +1,16 @@
+﻿using server.Models.DTOs.User;
+
+namespace server.Services.Interfaces
+{
+    public interface IStudentService
+    {
+        Task<UserResponseDto> RegisterAsync(RegisterUserDto dto, string firebaseUid);
+        Task<UserResponseDto?> GetByIdAsync(string id);
+
+        Task<IEnumerable<UserResponseDto>> GetAllAsync();
+
+        Task<UserResponseDto?> UpdateAsync(string id, UpdateUserDto dto);
+
+        Task<bool> DeactivateAsync(string id);
+    }
+}
