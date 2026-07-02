748113b6350ec44a011417a9867461bae897c21c
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-07-03 01:55:15 +0800

    feat: add interface layer for program headrepository

--- server/Repositories/Interfaces/IProgramHeadRepository.cs
+++ server/Repositories/Interfaces/IProgramHeadRepository.cs
@@ -1,7 +1,17 @@
+using server.Models.Entities;
+
 namespace server.Repositories.Interfaces
 {
     public interface IProgramHeadRepository
     {
-        
+        Task<IEnumerable<ProgramHead>> GetAllProgramHeadsAsync();
+
+        Task<ProgramHead?> GetProgramHeadByIdAsync(string id);
+
+        Task<bool> CreateProgramHeadAsync(ProgramHead programHead);
+
+        Task<bool> UpdateProgramHeadAsync(ProgramHead programHead);
+
+        Task<bool> DeleteProgramHeadAsync(string id);
     }
 }
