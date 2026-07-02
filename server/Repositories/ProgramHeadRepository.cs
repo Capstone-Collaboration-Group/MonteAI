6e4c2286b3bf779f0f1510172e7ea72dc1aa2c09
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-07-03 01:53:10 +0800

    feat: add data access layer for ProgramHead

--- server/Repositories/ProgramHeadRepository.cs
+++ server/Repositories/ProgramHeadRepository.cs
@@ -1,9 +1,60 @@
+using Microsoft.EntityFrameworkCore;
+using server.Data;
+using server.Models.Entities;
 using server.Repositories.Interfaces;
 
 namespace server.Repositories
 {
     public class ProgramHeadRepository : IProgramHeadRepository
     {
+        private readonly AppDbContext _db;
+
+        public ProgramHeadRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        // GetAllProgramHeadsAsync
+        public async Task<IEnumerable<ProgramHead>> GetAllProgramHeadsAsync() => await _db.ProgramHeads.ToListAsync();
+
+        // GetProgramHeadByIdAsync
+        public async Task<ProgramHead?> GetProgramHeadByIdAsync(string id) => await _db.ProgramHeads.FindAsync();
+
+        // CreateProgramHeadAsync
+        public async Task<bool> CreateProgramHeadAsync(ProgramHead programHead)
+        {
+            var result = await _db.ProgramHeads.FindAsync(programHead.Id);
+            if (result != null) return false;
+
+            await _db.ProgramHeads.AddAsync(programHead);
+            await _db.SaveChangesAsync();
+
+            return true;
+
+        }
         
+        // UpdateProgramHeadAsync
+        public async Task<bool> UpdateProgramHeadAsync(ProgramHead programHead)
+        {
+            var result = await _db.ProgramHeads.FindAsync(programHead.Id);
+            if (result == null) return false;
+
+            _db.ProgramHeads.Update(programHead);
+            await _db.SaveChangesAsync();
+
+            return true;
+        }
+
+        // DeleteProgramHeadAsync
+        public async Task<bool> DeleteProgramHeadAsync(string id)
+        {
+            var result = await _db.ProgramHeads.FindAsync(id);
+            if (result == null) return false;
+
+            _db.ProgramHeads.Remove(result);
+            await _db.SaveChangesAsync();
+            return true;
+        }
+
     }
 }
