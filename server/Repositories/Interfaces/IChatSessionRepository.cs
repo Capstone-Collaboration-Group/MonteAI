ff88e674813f36bee1cc3ff43a8957c26f75a985
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-07-03 01:56:16 +0800

    feat: add interface layer for ChatSessionRepository

---
+++ server/Repositories/Interfaces/IChatSessionRepository.cs
@@ -0,0 +1,17 @@
+﻿using server.Models.Entities;
+
+namespace server.Repositories.Interfaces
+{
+    public interface IChatSessionRepository
+    {
+        Task<IEnumerable<ChatSession>> GetAllChatSessionsAsync(string userId);
+
+        Task<ChatSession?> GetChatSessionByIdAsync(Guid id);
+
+        Task<bool> CreateChatSessionAsync(ChatSession chatSession);
+
+        Task<bool> UpdateChatSessionAsync(ChatSession chatSession);
+
+        Task<bool> DeleteChatSessionAsync(Guid id);
+    }
+}
