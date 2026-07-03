18eee0b801cbd725dff174141e50b2f9fcfa07e9
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-07-04 03:00:33 +0800

    feat: add interface for chat session service

--- server/Services/Interfaces/IChatSessionService.cs
+++ server/Services/Interfaces/IChatSessionService.cs
@@ -1,7 +1,21 @@
-﻿namespace server.Services.Interfaces
+﻿using server.Models.DTOs.ChatSession;
+
+namespace server.Services.Interfaces
 {
     public interface IChatSessionService
     {
+        // GetAllAsync
+        Task<IEnumerable<ChatSessionResponseDto>> GetAllAsync(string userId);
+        // GetByIdAsync
+        Task<ChatSessionResponseDto?> GetByIdAsync(Guid id);
+        // CreateAsync
+        Task<ChatSessionResponseDto> CreateAsync(CreateChatSessionDto chatSession);
+        // UpdateAsync
+        Task<bool> UpdateAsync(UpdateChatSessionDto chatSession);
 
+        // DeleteAsync
+        Task<bool> DeleteAsync(Guid id);
+
+        
     }
 }
