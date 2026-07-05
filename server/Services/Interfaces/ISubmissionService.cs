e96d8e90446d360a223cdce846c866dd9f6e695f
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-07-05 16:17:04 +0800

    feat: add interface layer for Submission Service

---
+++ server/Services/Interfaces/ISubmissionService.cs
@@ -0,0 +1,19 @@
+﻿using server.Models.DTOs.Submission;
+
+namespace server.Services.Interfaces
+{
+    public interface ISubmissionService
+    {
+        Task<IEnumerable<SubmissionResponseDto>> GetAllAsync();
+
+        Task<IEnumerable<SubmissionResponseDto>> GetAllByUserIdAsync(string studentNumber, Guid ThesisId);
+
+        Task<SubmissionResponseDto?> GetByIdAsync(Guid submissionId);
+
+        Task<SubmissionResponseDto?> CreateAsync(CreateSubmissionDto dto);
+
+        Task<SubmissionResponseDto?> UpdateAsync(UpdateSubmissionDto dto, Guid submissionId);
+
+        Task<bool> DeleteAsync(Guid submissionId);
+    }
+}
