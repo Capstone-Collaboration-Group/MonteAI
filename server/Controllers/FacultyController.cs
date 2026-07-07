9671a5cfd030f43a4ca9b43607f71110047fef9e
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-07-07 10:40:55 +0800

    feat: add api endpoints

--- server/Controllers/FacultyController.cs
+++ server/Controllers/FacultyController.cs
@@ -1,6 +1,8 @@
 using System;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using server.Models.DTOs.Faculty;
 using server.Models.DTOs.User;
 using server.Services.Interfaces;
     
@@ -8,14 +10,53 @@ namespace server.Controllers
 { 
     [ApiController]
     [Route("api/v1/[controller]")]
-    public class FacultyController : ControllerBase {
+    public class FacultyController 
+    (   IFacultyService _service,
+        ILogger<FacultyController> _logger
+    ) : ControllerBase 
+    {
+        [HttpGet("faculties")]
+        public async Task<IActionResult> GetAllFaculties()
+        {
+            var result = await _service.GetAllAsync();
+            if (result is null) return BadRequest("Bad Request. Try Again Later...");
 
-    //<-- Inherited from Microsoft.AspNetCore.Mvc
-    private readonly ILogger<FacultyController> _logger;
+            _logger.LogInformation("Fetched {count} Faculties", result.Count());
+            return Ok(result);
+        }
+        [HttpGet("faculties/{id}")]
+        public async Task<IActionResult> GetFacultyById(string id)
+        {
+            var result = await _service.GetByIdAsync(id);
+            if (result is null) return NotFound($"Faculty with Id {id} is not found");
 
-    // Constructor
-    public FacultyController(ILogger<FacultyController> logger) { 
-        _logger = logger;
+            _logger.LogInformation("Faculty With Id {id} fetched.", result.Id);
+            return Ok(result);
+
+        }
+        // No Faculty Creation POST Method since it will be handled in the registration Authcontroller
+
+        [HttpPatch]
+        public async Task<IActionResult> UpdateFaculty([FromBody] UpdateFacultyDto dto)
+        {
+            var result = await _service.UpdateAsync(dto);
+            if(result)
+            {
+                _logger.LogInformation("Faculty Updated Successfully");
+                return Ok(new { Message = "Faculty Updated Successfully", result });
+            }
+            return BadRequest(new {Message = "Faculty Update Not Successful", result});
+        }
+        [HttpDelete("faculties/delete{id}")]
+        public async Task<IActionResult> DeleteFaculty(string id)
+        {
+            var result = await _service.DeleteAsync(id);
+            if (result)
+            {
+                _logger.LogInformation("Faculty Deleted Successfully");
+                return Ok(new { Message = "Faculty Deleted Successfully", result });
+            }
+            return BadRequest(new { Message = "Faculty Delete Not Successful", result });
+        }
     }
-}
 }
