a5549430317577a8649d241ec57541842a35a801
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-07-08 10:06:57 +0800

    refactor: add id string argument

--- server/Services/User/ProgramHeadService.cs
+++ server/Services/User/ProgramHeadService.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Mapster;
 using server.Models.DTOs.ProgramHead;
+using server.Models.DTOs.User;
 using server.Models.Entities;
 using server.Repositories.Interfaces;
 using server.Services.Interfaces;
@@ -13,32 +14,33 @@ namespace server.Services.User
     IMapper _mapper
     ): IProgramHeadService
     {
-        public async Task<IEnumerable<ProgramHeadResponseDto>> GetAllAsync()
+        public async Task<IEnumerable<UserResponseDto>> GetAllAsync()
         {
         var result = await _repo.GetAllProgramHeadsAsync();
 
-        var responseDto = _mapper.Map<IEnumerable<ProgramHeadResponseDto>>(result);
+        var responseDto = _mapper.Map<IEnumerable<UserResponseDto>>(result);
 
         return responseDto;
         }
-        public async Task<ProgramHeadResponseDto> GetByIdAsync(string id)
+        public async Task<UserResponseDto?> GetByIdAsync(string id)
         {
             var result = await _repo.GetProgramHeadByIdAsync(id);
-            var responseDto = _mapper.Map<ProgramHeadResponseDto>(result);
+            var responseDto = _mapper.Map<UserResponseDto>(result);
             return responseDto;
         }
-        public async Task<bool> CreateAsync(CreateProgramHeadDto createDto)
+        public async Task<bool> CreateAsync(RegisterUserDto createDto)
         {
+            Console.WriteLine($"Id is {createDto.Id}");
             var programHead = _mapper.Map<ProgramHead>(createDto);
             var result = await _repo.CreateProgramHeadAsync(programHead);
 
             return result;
         }
-        public async Task<bool> UpdateAsync(UpdateProgramHeadDto updateDto)
+        public async Task<bool> UpdateAsync(UpdateProgramHeadDto updateDto, string id)
         {
             var programHead = _mapper.Map<ProgramHead>(updateDto);
 
-            var result = await _repo.UpdateProgramHeadAsync(programHead);
+            var result = await _repo.UpdateProgramHeadAsync(programHead, id);
 
             return result;
         }
