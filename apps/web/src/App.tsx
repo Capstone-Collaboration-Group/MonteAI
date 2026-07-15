330d199286f18d4cfbf7c654c3969c3fea0959a7
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-07-16 03:31:36 +0800

    fix: resolve missing Home page import

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,14 +1,13 @@
 
 import "./App.css";
-import { Button, Card, Toaster} from "@monteai/ui";
+import  Home  from "./pages/Home";
+import { Toaster} from "@monteai/ui";
 
 function App() {
   return (
  <div>
       <Toaster />
-      <Card>
-        <Button onClick={() => alert("clicked")}>Save</Button>
-      </Card>
+      <Home />
     </div>
   )
 }
