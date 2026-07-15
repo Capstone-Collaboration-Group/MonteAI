3b25ec998f3d6c8c1ec2af31107e13f679aadc8e
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-07-16 04:41:51 +0800

    refactor: moved ChatPreview component to packages/ui

--- apps/desktop/src/App.tsx
+++ apps/desktop/src/App.tsx
@@ -1,10 +1,12 @@
 // apps/desktop/src/App.tsx (or wherever your renderer root component is)
-import { Button, Card, Toaster } from "@monteai/ui";
+import { Button, Card, Toaster, ChatPreview } from "@monteai/ui";
+
 
 function App() {
   return (
     <div>
         <h1>hello?</h1>
+        <ChatPreview />
       <Toaster />
       <Card>
         <Button onClick={() => alert("clicked")}>Save</Button>
--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,12 +1,12 @@
 
 import "./App.css";
 import  Home  from "./pages/Home";
-import { Toaster} from "@monteai/ui";
 
 function App() {
   return (
  <div>
-      <Toaster />
+
+
       <Home />
     </div>
   )
