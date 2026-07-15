0e857f1f53e1dc51dd87b08be2a2f77b4d622077
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-07-16 07:52:02 +0800

    refactor: removed test button

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -3,12 +3,12 @@ import Hero from "../components/Hero";
 import Features from "../components/Features";
 import ChatPreview from "../components/ChatPreview";
 import Footer from "../components/Footer";
-import { Button } from "@monteai/ui";
+
 
 export default function Home() {
   return (
     <main className="w-full bg-linear-to-b from-green-50 to-white">
-      <Button />
+
       <Navbar />
       <div className="mx-auto flex max-w-7xl flex-col px-6 py-8 lg:px-8">
         <Hero />
