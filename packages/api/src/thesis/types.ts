f37de5c08b7b2304b4c98e8675009fe9365fd0c5
Charles Bernard Balaguer <tnts202603047@iacademy.edu.ph>
2026-07-18 07:21:53 +0000

    add interface types for thesis

--- packages/api/src/thesis/types.ts
+++ packages/api/src/thesis/types.ts
@@ -1,11 +1,15 @@
 import type { 
     SubmitThesisDto,
     UpdateThesisDto,
-    ThesisResponseDto
+    ThesisResponseDto,
+    ThesisResponseListDto
 } from "@monteai/types";
 export interface ThesisService { 
     submitThesis(dto: SubmitThesisDto): Promise<ThesisResponseDto>;
-    updateThesis(thesisID: string, dto: UpdateThesisDto): Promise<boolean>;
     getThesis(thesisId: string): Promise<ThesisResponseDto | null>;
+    getTheses(): Promise<ThesisResponseListDto>;
+    updateThesis(thesisId: string, dto: UpdateThesisDto): Promise<boolean>;
+    updateThesisStatus(thesisId: string, status: string): Promise<boolean>;
     deleteThesis(thesisId: string): Promise<boolean>;
 }
+
