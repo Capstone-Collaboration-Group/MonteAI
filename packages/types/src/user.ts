951cef273e2f5cb8bb792324f595d79dafdcc000
Liyo Wang <liyowang31@gmail.com>
2026-07-18 10:07:40 +0800

    feat/added-user-interfacefile-and-response-dto-list

--- packages/types/src/user.ts
+++ packages/types/src/user.ts
@@ -1,5 +1,45 @@
-export interface User { 
-    id: string;
-    email: string;
-    displayName: string;
+
+export interface RegisterUserDto {
+  id: string;
+  email: string;
+  firstName: string;
+  middleInitial: string;
+  lastName: string;
+  suffix?: string;
+  role: string;
+
+  // Student-specific
+  studentNumber?: string;
+  groupId?: string;
+  position?: string;
+  institute?: string;
+  program?: string;
+  yearLevel?: number;
+  section?: string;
+
+  // Program Head-specific
+  programHandled?: string;
+}
+
+export interface UpdateUserDto {
+  email?: string;
+  firstName?: string;
+  middleInitial?: string;
+  lastName?: string;
+  suffix?: string;
+  role?: string;
+  isActive?: boolean;
+}
+
+export interface UserResponseDto {
+  id: string;
+  email: string;
+  firstName: string;
+  middleInitial: string;
+  lastName: string;
+  suffix?: string;
+  role: string;
+  isActive?: boolean;
+  createdAt: string;
+  updatedAt: string;
 }
