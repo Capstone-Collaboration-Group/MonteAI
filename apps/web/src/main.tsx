2fcaf75d55b4da49ad3f34b03125eb1cb6a95fc4
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-07-22 09:03:04 +0800

    fix: resolve merge confict

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,24 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { AuthProvider } from "@monteai/hooks";
 import './index.css'
 import App from './App.tsx'
-import { auth } from './lib/firebase';
 import './lib/firebaseServices.ts';
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AuthProvider auth={auth}>
-      <App />
-    </AuthProvider>
-  </StrictMode>,
-)
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import { createApiClient } from "@monteai/api";
-
 import "./index.css";
-import App from "./App";
+
 
 export const api = createApiClient({
   baseURL: import.meta.env.VITE_API_BASE_URL,
