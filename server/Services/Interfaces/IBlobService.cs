a6b9ea86974b9601817f4668bb8c1b5cd0c573cc
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-08-02 23:42:19 +0800

    feat: add service for azure blob storage

---
+++ server/Services/Interfaces/IBlobService.cs
@@ -0,0 +1,10 @@
+﻿
+namespace server.Services.Interfaces
+{
+    public interface IBlobService
+    {
+        Task<string> UploadAsync(Stream fileStream, string fileName, string contentType);
+        Task DeleteAsync(string blobUrl);
+        string GenerateSasUrl(string blobUrl, int expiryMinutes);
+    }
+}
