b2292ae785767eada26c316c4a2df28db3ca8fb6
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-08-13 22:45:25 +0800

    chore(BREAKING CHANGE): added configurations for PDF viewer implmentatoin

--- apps/desktop/vite.main.config.ts
+++ apps/desktop/vite.main.config.ts
@@ -1,4 +1,10 @@
-import { defineConfig } from 'vite';
+// apps/desktop/vite.main.config.ts
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ["canvas"],
+    },
+  },
+});
