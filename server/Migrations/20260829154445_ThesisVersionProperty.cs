983cceefc7f5c0112539a12e85bdac8f515f10f5
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-08-30 19:37:56 +0800

    chore: add new migration for altering ThesisVersion table

---
+++ server/Migrations/20260829154445_ThesisVersionProperty.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThesisVersionProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_ThesisVersions_ThesisVersionId",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_ThesisVersionId",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "ThesisVersionId",
+                table: "Reviews");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ChangeNote",
+                table: "ThesisVersions",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UploadedById",
+                table: "ThesisVersions",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChangeNote",
+                table: "ThesisVersions");
+
+            migrationBuilder.DropColumn(
+                name: "UploadedById",
+                table: "ThesisVersions");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ThesisVersionId",
+                table: "Reviews",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_ThesisVersionId",
+                table: "Reviews",
+                column: "ThesisVersionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_ThesisVersions_ThesisVersionId",
+                table: "Reviews",
+                column: "ThesisVersionId",
+                principalTable: "ThesisVersions",
+                principalColumn: "Id");
+        }
+    }
+}
