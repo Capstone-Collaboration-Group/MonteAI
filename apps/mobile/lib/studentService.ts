84d6b39183ee647cbaadc8996eb15ed1aedaf4d5
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-09-08 01:25:52 +0800

    Update apps/mobile/lib/studentService.ts

--- apps/mobile/lib/studentService.ts
+++ apps/mobile/lib/studentService.ts
@@ -1,5 +1,6 @@
 // apps/mobile/lib/studentService.ts
 import { createApiClient, createStudentService } from "@monteai/api";
+import { getAuthToken } from "./authService";
 
 const baseURL =
   process.env.EXPO_PUBLIC_API_BASE_URL ?? "http://192.168.100.9:5084/api/v1";
@@ -7,6 +8,6 @@ const baseURL =
 const useMock =
   (process.env.EXPO_PUBLIC_USE_MOCK ?? (__DEV__ ? "true" : "false")) === "true";
 
-const client = createApiClient({ baseURL });
+const client = createApiClient({ baseURL, getAuthToken });
 
 export const studentService = createStudentService(client, useMock);
