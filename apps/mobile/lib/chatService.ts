6a40c89ddcec5200def700b95f04b64e472220cd
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-09-08 01:25:45 +0800

    Update apps/mobile/lib/chatService.ts

--- apps/mobile/lib/chatService.ts
+++ apps/mobile/lib/chatService.ts
@@ -1,5 +1,6 @@
 // apps/mobile/lib/chatService.ts
 import { createApiClient, createChatService } from "@monteai/api";
+import { getAuthToken } from "./authService";
 
 const baseURL =
   process.env.EXPO_PUBLIC_API_BASE_URL ?? "http://192.168.100.9:5084/api/v1";
@@ -7,6 +8,6 @@ const baseURL =
 const useMock =
   (process.env.EXPO_PUBLIC_USE_MOCK ?? (__DEV__ ? "true" : "false")) === "true";
 
-const client = createApiClient({ baseURL });
+const client = createApiClient({ baseURL, getAuthToken });
 
 export const chatService = createChatService(client, useMock);
